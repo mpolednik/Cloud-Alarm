e3149d396b651dcb21029f986b551f4b52f2e87a
tomasskopal <tomasskopal01@gmail.com>
2015-03-29 20:56:41 +0200

    Add any routes but only GET method are functional

--- Cloud-alarm-server/app.js
+++ Cloud-alarm-server/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var db = require('./db/MongoDbClient'); // open db connection
+var alarmRoutes = require('./routes/AlarmRoutes');
 
 var app = express();
 
@@ -14,16 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/alarms', alarmRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
