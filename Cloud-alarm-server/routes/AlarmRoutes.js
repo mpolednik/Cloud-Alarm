8b12022dc7b39f38a2f8c2cf5f4aa04530673a8b
tomasskopal <tomasskopal01@gmail.com>
2015-03-29 22:30:03 +0200

    All methods from apiary are implemented. (getAll, getById, post, delete)

--- Cloud-alarm-server/routes/AlarmRoutes.js
+++ Cloud-alarm-server/routes/AlarmRoutes.js
@@ -9,23 +9,32 @@ router.get('/', function(req, res) {
     });
 });
 
-router.post('/', function(req, res) {
-    console.log(req.body);
-    new Alarm({
-        _id: req.body.id,
-        title: req.body.title,
-        hour: req.body.header,
-        minute: req.body.minute,
-        enabled: req.body.enabled,
-        repeat: req.body.repeat,
-        days: req.body.days
-    }).save();
-    res.status(201).send("created");
-});
+router.post('/', function(req, res, next) {
+        if(req.body.title === undefined) { // Better one validation than nothing
+            console.log(req.body);
+            res.status(400).send("Title is missing");
+        } else {
+            next();
+        }
+    },
+    function(req, res) {
+        console.log(req.body);
+        new Alarm({
+            _id: req.body.id,
+            title: req.body.title,
+            hour: req.body.header,
+            minute: req.body.minute,
+            enabled: req.body.enabled,
+            repeat: req.body.repeat,
+            days: req.body.days
+        }).save();
+        res.status(201).send("created");
+    }
+);
 
 router.get('/:id', function(req, res, next) { // example http://localhost:3000/alarms/1
-        if (req.params.id === null) {
-            res.status(400).send("Id is null or is not a number");
+        if (req.params.id === undefined) {
+            res.status(400).send("Id is undefined");
         } else {
             next();
         }
@@ -34,6 +43,21 @@ router.get('/:id', function(req, res, next) { // example http://localhost:3000/a
         var id = req.params.id;
         Alarm.find({ _id : id}, function(err,data) {
             res.status(200).json(data);
+        });
+    }
+);
+
+router.delete('/:id', function(req, res, next) { // example http://localhost:3000/alarms/1
+        if (req.params.id === undefined) {
+            res.status(400).send("Id is undefined");
+        } else {
+            next();
+        }
+    },
+    function(req, res) {
+        var id = req.params.id;
+        Alarm.findOneAndRemove({ _id : id}, function() {
+            res.status(200).send("Removed");
         });
     }
 );
