60db64208b7c7ce820be2d833bb2d0da7863179e
Martin Polednik <mpolednik@redhat.com>
2015-04-02 15:17:43 +0200

    Add auto selection for repeat days in alarm
    Alarm repeat selection should be (in edit mode) pre-selected to what
    was already selected before. This is implemented by segue and
    unwinding, where data is passed around the three (one containered)
    views.

--- Cloud Alarm/AlarmAddEditViewController.swift
+++ Cloud Alarm/AlarmAddEditViewController.swift
@@ -13,13 +13,33 @@ import ObjectMapper
 class AlarmAddEditViewController: UIViewController {
     
     var alarm: Alarm? = nil
+    var tableViewController: AlarmAddEditTableViewController?
+    var selectedDays: [Int] = [] {
+        didSet {
+            tableViewController!.selectedDays = selectedDays
+        }
+    }
     
     @IBOutlet weak var timepicker: UIDatePicker!
-    @IBOutlet weak var tableView: UIView!
+    
+    @IBAction func unwindRepeat(segue: UIStoryboardSegue) {
+        let controller: RepeatPickerViewController = segue.sourceViewController as RepeatPickerViewController
+        self.selectedDays = controller.selectedDays
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "embededAlarmMenu" {
+            let destination: AlarmAddEditTableViewController = segue.destinationViewController as AlarmAddEditTableViewController
+            destination.selectedDays = self.selectedDays
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableViewController = childViewControllers[0] as? AlarmAddEditTableViewController
+        
         if let alarm = alarm {
             // How do we set the timepicker hour and minute? Since constructing a NSDate object is quite difficult,
             // we can simply use current NSDate, create calendar based on that date and initiate it's components.
@@ -32,13 +52,27 @@ class AlarmAddEditViewController: UIViewController {
             if let newDate = calendar.dateFromComponents(components) {
                 self.timepicker.setDate(newDate, animated: true)
             }
-        } else {
-
+            
+            if let repeat = alarm.days {
+                self.selectedDays = repeat
+            }
         }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+class AlarmAddEditTableViewController: UITableViewController {
+    
+    var selectedDays: [Int] = []
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showRepeatPicker" {
+            let destination: RepeatPickerViewController = segue.destinationViewController as RepeatPickerViewController
+            destination.selectedDays = self.selectedDays
+        }
     }
 }
--- Cloud Alarm/RepeatPickerViewController.swift
+++ Cloud Alarm/RepeatPickerViewController.swift
@@ -12,6 +12,15 @@ import UIKit
 class RepeatPickerViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
     
     let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    var selectedDays: [Int] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for day in self.selectedDays {
+            self.tableView.selectRowAtIndexPath(NSIndexPath(forRow: day, inSection: 0), animated: false, scrollPosition: .None)
+        }
+    }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return days.count
@@ -21,5 +30,15 @@ class RepeatPickerViewController: UITableViewController, UITableViewDataSource, 
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("RepeatCell") as UITableViewCell
         cell.textLabel!.text = days[indexPath.row]
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.selectedDays.append(indexPath.row)
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        if let index = find(self.selectedDays, indexPath.row) {
+            self.selectedDays.removeAtIndex(index)
+        }
     }
 }
